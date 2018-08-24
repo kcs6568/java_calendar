151bcdf7be301e9cc52e06f349021cf6960272be
CSKIM <CSKIM@DESKTOP-PVMB52F>
2018-08-24 21:26:02 +0900

    Update : sum two numbers

--- src/study/calendar/Calendar.java
+++ src/study/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package study.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -9,6 +11,13 @@ public class Calendar {
 		System.out.println("  8    9  10  11  12  13  14");
 		System.out.println("15  16  17  18  19  20  21");
 		System.out.println("22  23  24  25  26  27  28");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("두 수를 입력하세요.");
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		System.out.println("두 수의 합은 "+(n1+n2)+"입니다.");
+		sc.close(); //스캐너 사용종료.
 	}
 }
 
