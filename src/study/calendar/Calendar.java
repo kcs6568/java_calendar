cd43a7eaaf94bf8262833d133982e32525f33086
CSKIM <CSKIM@DESKTOP-PVMB52F>
2018-08-24 20:59:31 +0900

    Init Calendar Project

--- src/study/calendar/Calendar.java
+++ src/study/calendar/Calendar.java
@@ -3,8 +3,11 @@ package study.calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
 }
+
+//team->share project로 git과 연동함
+//show in->system explorer에 들어가면 git파일 생성됨.
+//team -> add to index하면 클래스파일에 +모양이 생김. 커밋할 준비가 되었다는 의미.
+//staged changes : 커밋 될 파일들 출력
